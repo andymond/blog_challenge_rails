a4b63bc5f58cd8fcbe5fa65e197bbba85a775ec2
Andrew Dymond <andy.dymond@gmail.com>
2018-06-01 11:14:58 -0600

    Add user can log out spec

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -13,4 +13,15 @@ describe 'as a user' do
     expect(current_path).to eq(dashboard_index_path)
     expect(page).to have_content("Welcome larry")
   end
+
+  it 'can log out of account' do
+    user = User.create(username: "larry", email: "test@email.com", password: "password")
+    allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(user)
+
+    visit dashboard_index_path
+    click_on "log out"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You have logged out.")
+  end
 end
